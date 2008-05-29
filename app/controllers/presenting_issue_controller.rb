415c907d209b5c053a54929d65469f15b650ea52
simonm <simonm@a365057f-8f2b-0410-9532-a223011ada3c>
2008-05-29 15:49:05 +0000

    Tweaked the Presenting Issue controller
    git-svn-id: https://jump.sla-mobile.com/svn_prototypes/lifeline/trunk@89 a365057f-8f2b-0410-9532-a223011ada3c

--- app/controllers/presenting_issue_controller.rb
+++ app/controllers/presenting_issue_controller.rb
@@ -1,3 +1,8 @@
 class PresentingIssueController < ApplicationController
   active_scaffold
+
+  active_scaffold :presenting_issue do |config|
+    # disable the create/edit fucntionality for the presenting issues
+    config.columns.exclude :call
+  end
 end
