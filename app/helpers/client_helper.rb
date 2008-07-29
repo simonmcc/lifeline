6f0b888d0d3846c6ef40003193c2739031c8007d
simonm <simonm@a365057f-8f2b-0410-9532-a223011ada3c>
2008-07-29 08:54:18 +0000

    Tweaked the dob form so that it works :-)
    git-svn-id: https://jump.sla-mobile.com/svn_prototypes/lifeline/trunk@163 a365057f-8f2b-0410-9532-a223011ada3c

--- app/helpers/client_helper.rb
+++ app/helpers/client_helper.rb
@@ -7,8 +7,7 @@ module ClientHelper
 
   def dob_form_column(record, input_name)
     #date_select(record, input_name, :order => [:day, :month, :year], :start_year => 1970, :end_year => Date.today.year-5 )
-    date_select(record, 'dob', :start_year => 1920, :end_year => Date.today.year, :include_blank => true )
-
+    date_select  :record, 'dob', :order => [:day, :month, :year], :start_year => 1970, :end_year => Date.today.year 
   end
 
 end
