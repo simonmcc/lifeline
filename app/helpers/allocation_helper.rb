d6a550a5772fdfe90622d2d239325778932465a2
nokane <nokane@a365057f-8f2b-0410-9532-a223011ada3c>
2008-08-13 12:54:10 +0000

    minor text changes
    git-svn-id: https://jump.sla-mobile.com/svn_prototypes/lifeline/trunk@164 a365057f-8f2b-0410-9532-a223011ada3c

--- app/helpers/allocation_helper.rb
+++ app/helpers/allocation_helper.rb
@@ -2,7 +2,10 @@ module AllocationHelper
 
   def date_intervention_ended_form_column(record, input_name)
     #date_select(record, input_name, :order => [:day, :month, :year], :start_year => 1970, :end_year => Date.today.year-5 )
-#    date_select(record, input_name, :order => [:day, :month, :year], :start_year => 1970, :end_year => Date.today.year )
+    date_select :record, 'date_intervention_ended', :order => [:day, :month, :year], :start_year => 1970, :end_year => Date.today.year, :include_blank => true
+
+    #date_select  :record, 'dob', :order => [:day, :month, :year], :start_year => 1970, :end_year => Date.today.year
+
   end
   
 
